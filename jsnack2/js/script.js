d02d2c1abb658e442c13ae239b3a1f027927b901
albertoferri <falberto99@gmail.com>
2024-02-12 14:04:14 +0100

    definisco i due array e stampo in console

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,3 +1,11 @@
 // Snack 2:
 // Scrivi una funzione che fonda due array presi come parametri (date per scontato che abbiano lo stesso numero di elementi) 
 // prendendo alternativamente gli elementi da uno e dall’altro
+
+// definisco le variabili
+const array1 = ['rosso', 'verde', 'blu'];
+const array2 = [1, 2, 3];
+
+// stampo in console i due array
+console.log(array1); 
+console.log(array2);
