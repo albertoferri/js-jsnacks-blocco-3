3bae75d39da02894731f8376cc81717d8829e641
albertoferri <falberto99@gmail.com>
2024-02-12 18:44:52 +0100

    creata funzione rispondente al click del bottone

--- jsnack5/js/script.js
+++ jsnack5/js/script.js
@@ -1,2 +1,14 @@
 // Snack 5 (bonus):
 // Scrivi una funzione che accetti una stringa come parametro e restituisca la stessa stringa ma con le iniziali delle parole tutte in maiuscolo
+
+function converti() {
+    const inputElement = document.getElementById("inputStringa");
+    // prendo il valore della stringa inserita
+    const inputStringa = inputElement.value;
+    const risultatoElement = document.getElementById("risultato");
+    
+    console.log(inputStringa)
+
+    // stampo in pagina la frase con le iniziali maiuscole
+    // risultatoElement.innerHTML = "Risultato: " + inizialiMaiuscole(inputStringa);
+}
