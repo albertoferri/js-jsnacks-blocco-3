28891c06b1d6f80f1e46424934e57365528ee7ec
albertoferri <falberto99@gmail.com>
2024-02-12 13:54:36 +0100

    inizio snack 2

---
+++ jsnack2/js/script.js
@@ -0,0 +1,3 @@
+// Snack 2:
+// Scrivi una funzione che fonda due array presi come parametri (date per scontato che abbiano lo stesso numero di elementi) 
+// prendendo alternativamente gli elementi da uno e dall’altro
