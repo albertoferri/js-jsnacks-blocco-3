e04d5b6450ef5e5db3e2fa930561526cff7bce93
albertoferri <falberto99@gmail.com>
2024-02-12 16:30:57 +0100

    aggiunta funzione e stampato in console il risultato

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,3 +1,18 @@
 // Snack 3:
 // Scrivi una funzione che accetti tre argomenti:
 // un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array).
+
+// determino le variabili
+const arrayNumeri = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Scrivi una funzione che accetti tre argomenti:
+const risultato = tagliaArray(arrayNumeri, 2, 7);
+
+function tagliaArray(arrayNumeri, Min, Max) {  
+    // slice permette di trascrivere in un altro array dei valori contenenti nel primo array, senza modificarlo
+    // utilizzo max + 1 per trascrivere fino al numero max compreso
+    const nuovoArray = arrayNumeri.slice(Min, Max + 1);
+
+    return nuovoArray;
+}
+
+console.log(risultato)
