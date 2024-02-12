a25af6fb4b97fc6a356e9e95c1f59a45d4e11505
albertoferri <falberto99@gmail.com>
2024-02-12 14:10:20 +0100

    aggiungo funzione per mixare i due array

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -5,7 +5,25 @@
 // definisco le variabili
 const array1 = ['rosso', 'verde', 'blu'];
 const array2 = [1, 2, 3];
+// aggiungo variabile per mix
+const mixed = mixedArrays(array1, array2);
 
 // stampo in console i due array
 console.log(array1); 
 console.log(array2);
+
+// funzione per unire i due array
+function mixedArrays(array1, array2) {
+    const fusedArray = [];
+
+    for (let i = 0; i < array1.length; i++) {
+        fusedArray.push(array1[i]);
+        fusedArray.push(array2[i]);
+    }
+
+    return fusedArray;
+}
+
+// stampo in pagina l'array fuso
+document.querySelector("#fused-array").innerHTML = "il tuo array mixato è: " + mixed;
+console.log(mixed); 
